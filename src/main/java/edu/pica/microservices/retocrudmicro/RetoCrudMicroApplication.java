80da95a59948f9dbee4499486a5e17a34a200885
Tito Andrés Maturana <tamatu@gmail.com>
2022-09-29 07:52:37 -0500

    first commit

---
+++ src/main/java/edu/pica/microservices/retocrudmicro/RetoCrudMicroApplication.java
@@ -0,0 +1,13 @@
+package edu.pica.microservices.retocrudmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoCrudMicroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoCrudMicroApplication.class, args);
+    }
+
+}
